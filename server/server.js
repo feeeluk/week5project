6c2173ae42f46ca5c79bf3de838fd363b4bb528b
Macho1878 <macho1878@LAPTOP-B096Q40N>
2024-06-04 09:51:22 +0100

    server-update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-import express from "express"
-import cors from "cors"
-const app = express()
-app.use(cors())
+import express from "express";
+import cors from "cors";
 
-app.listen ('8080', () => {
-    console.log('server listening on port: 8080')
+const PORT = 8080;
+
+const app = express();
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.json({ message: `root` });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
+/*needed for form
+app.get('/form', (request, response) => {
+    response.json({message: `form`})
 })
 
+app.post("/form", function (request, response) {
+    response.json({ message: "you sent this to me" });
+  });
+ 
+app.use(express.json());
+*/
